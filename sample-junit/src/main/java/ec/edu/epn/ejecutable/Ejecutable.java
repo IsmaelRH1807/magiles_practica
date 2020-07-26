3c515061d6bbad48acfccb138741bf193fe94e14
User <bryan.ortuno@epn.edu.ec>
2020-07-25 20:10:00 -0500

    Correccion errores

--- sample-junit/src/main/java/ec/edu/epn/ejecutable/Ejecutable.java
+++ sample-junit/src/main/java/ec/edu/epn/ejecutable/Ejecutable.java
@@ -6,6 +6,13 @@ public class Ejecutable {
     
     public static void main(String[] args) {
        
+    	System.out.println("********************************");
+    	System.out.println("Metodologías Ágiles");
+    	System.out.println("Pruebas con de integración con Jenkins y GitHub\n\n");
+    	System.out.println("INTEGRANTES: BRYAN ORTUÑO - ISMAEL RIVAS\n\n");
+    	System.out.println("CONEXIÓN EXITOSA\n");
+    	System.out.println("********************************");
+    	
        String nombre = "Bryan Galindo"; 
        String cedula = "1723695887";
         JFramePublicacion jpub = new JFramePublicacion(nombre,cedula);
